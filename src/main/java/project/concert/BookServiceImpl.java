0ffc257244a1ec37788e55639438a599883d9f2d
Ryan G <ryan.goh.2022@scis.smu.edu.sg>
2023-09-13 22:57:58 +0800

    copy over reference java files for concert

---
+++ src/main/java/project/concert/BookServiceImpl.java
@@ -0,0 +1,50 @@
+package csd.week5.book;
+
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class BookServiceImpl implements BookService {
+   
+    private BookRepository books;
+    
+
+    public BookServiceImpl(BookRepository books){
+        this.books = books;
+    }
+
+    @Override
+    public List<Book> listBooks() {
+        return books.findAll();
+    }
+
+    
+    @Override
+    public Book getBook(Long id){
+        return books.findById(id).orElse(null);
+    }
+    
+    @Override
+    public Book addBook(Book book) {
+        return books.save(book);
+    }
+    
+    @Override
+    public Book updateBook(Long id, Book newBookInfo){
+        return books.findById(id).map(book -> {book.setTitle(newBookInfo.getTitle());
+            return books.save(book);
+        }).orElse(null);
+
+    }
+
+    /**
+     * Remove a book with the given id
+     * Spring Data JPA does not return a value for delete operation
+     * Cascading: removing a book will also remove all its associated reviews
+     */
+    @Override
+    public void deleteBook(Long id){
+        books.deleteById(id);
+    }
+}
