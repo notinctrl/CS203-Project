5e1a5412d93d20dea4a0ec66b318f5af750a20ed
Ryan G <ryan.goh.2022@scis.smu.edu.sg>
2023-09-14 10:43:45 +0800

    concert imports added and added application code

--- src/main/java/project/concert/Concert.java
+++ src/main/java/project/concert/Concert.java
@@ -1,19 +1,21 @@
 package project.concert;
 
-import org.springframework.data.annotation.Id;
+import java.util.List;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import project.concert.*;
+import lombok.*;
 
+@Entity
 @Getter
 @Setter
 @ToString
