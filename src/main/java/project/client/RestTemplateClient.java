cc972fbcecc6f37481854a2f894bf813e96a5890
Ryan G <ryan.goh.2022@scis.smu.edu.sg>
2023-09-14 10:55:22 +0800

    fixed client side

--- src/main/java/project/client/RestTemplateClient.java
+++ src/main/java/project/client/RestTemplateClient.java
@@ -1,4 +1,4 @@
-package csd.week5.client;
+package project.client;
 
 
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
-import csd.week5.book.Book;
+import project.concert.*;
 
 @Component
 public class RestTemplateClient {
@@ -23,38 +23,38 @@ public class RestTemplateClient {
                 .build();
     }
     /**
-     * Get a book with given id
+     * Get a Concert with given id
      * 
      * @param URI
      * @param id
      * @return
      */
-    public Book getBook(final String URI, final Long id) {
-        final Book book = template.getForObject(URI + "/" + id, Book.class);
-        return book;
+    public Concert getConcert(final String URI, final Long id) {
+        final Concert Concert = template.getForObject(URI + "/" + id, Concert.class);
+        return Concert;
     }
 
     /**
-     * Add a new book
+     * Add a new Concert
      * 
      * @param URI
-     * @param newBook
+     * @param newConcert
      * @return
      */
-    public Book addBook(final String URI, final Book newBook) {
-        final Book returned = template.postForObject(URI, newBook, Book.class);
+    public Concert addConcert(final String URI, final Concert newConcert) {
+        final Concert returned = template.postForObject(URI, newConcert, Concert.class);
         
         return returned;
     }
 
     /**
-     * Get a book, but return a HTTP response entity.
+     * Get a Concert, but return a HTTP response entity.
      * @param URI
      * @param id
      * @return
      */
-    public ResponseEntity<Book> getBookEntity(final String URI, final Long id){
-        return template.getForEntity(URI + "/{id}", Book.class, Long.toString(id));
+    public ResponseEntity<Concert> getConcertEntity(final String URI, final Long id){
+        return template.getForEntity(URI + "/{id}", Concert.class, Long.toString(id));
     }
     
 }
