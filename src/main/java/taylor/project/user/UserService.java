cf5fc21d94e7a5a8e9e02d3cdc6c0606c1afea8a
Kwon DaHee <dahee.kwon.2022@scis.smu.edu.sg>
2023-09-16 15:49:52 +0800

    Added preauthorize for listUsers

--- src/main/java/taylor/project/user/UserService.java
+++ src/main/java/taylor/project/user/UserService.java
@@ -2,9 +2,13 @@ package taylor.project.user;
 
 import java.util.List;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+
 
 public interface UserService {
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     List<User> listUsers();
+    
     User getUser(String username);
     User addUser(User user);
 }
