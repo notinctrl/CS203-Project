dd0caddb663b9cc06d3c7c58d056aacb67db87e1
Juyoung Jang <jyjang.2022@scis.smu.edu.sg>
2023-09-26 16:38:57 +0800

    Added @RequestMapping for shopping cart in HTMLController.java

--- src/main/java/taylor/project/concertimage/HTMLController.java
+++ src/main/java/taylor/project/concertimage/HTMLController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -116,6 +117,11 @@ public class HTMLController {
     @GetMapping("/uploadStatus")
     public String uploadStatus() {
         return "uploadStatus";
+    }
+
+    @RequestMapping("/shoppingcart")
+    public String viewShoppingCart(){
+        return "shoppingcart";
     }
 
 }
