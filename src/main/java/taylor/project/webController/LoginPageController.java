d5d3c365ab85a07acc1af4faa33b11d334ee8d06
zyleehenry319 <henry.lee.2022@scis.smu.edu.sg>
2023-09-26 16:44:57 +0800

    created login controller

---
+++ src/main/java/taylor/project/webController/LoginPageController.java
@@ -0,0 +1,42 @@
+package taylor.project.webController;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginPageController {
+
+    @GetMapping("/login")
+    public String showLoginPage() {
+        // Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        // if (authentication==null||authentication instanceof AnonymousAuthenticationToken) {
+        //     return  "login";
+        // }
+
+        // return "redirect:/";
+        return "login";
+    }
+
+    @GetMapping("/home")
+    public String showHomePage() {
+        // Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        // if (authentication==null||authentication instanceof AnonymousAuthenticationToken) {
+        //     return  "login";
+        // }
+
+        // return "redirect:/";
+        return "home";
+    }
+
+    
+    @GetMapping("/index.html")
+    public String showIndexPage() {
+        // Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        // if (authentication==null||authentication instanceof AnonymousAuthenticationToken) {
+        //     return  "login";
+        // }
+
+        // return "redirect:/";
+        return "index";
+    }
+}
