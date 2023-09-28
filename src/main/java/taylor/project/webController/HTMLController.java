a35718f1009c77f82d67b7f2feab24cfe0217d82
Kwon DaHee <dahee.kwon.2022@scis.smu.edu.sg>
2023-09-28 17:34:33 +0800

    Moved html controller to web controller folder

---
+++ src/main/java/taylor/project/webController/HTMLController.java
@@ -0,0 +1,127 @@
+package taylor.project.webController;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.*;
+
+import org.apache.commons.io.FileUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import ch.qos.logback.core.util.FileUtil;
+import taylor.project.concert.Concert;
+import taylor.project.concert.ConcertService;
+
+@Controller
+public class HTMLController {
+
+    private ConcertService concertService;
+
+    public HTMLController(ConcertService cs){
+        this.concertService = cs;
+    }
+
+    //Save the uploaded file to this folder
+    private static String UPLOADED_FOLDER = "F://temp//";
+
+    @GetMapping("/index")
+    public String index(Model model) throws IOException{
+        List<Concert> concerts = concertService.listConcerts(); 
+        // Concert c1 = concerts.get(0);
+        // model.addAttribute("Concert_1_Name", c1.getConcertName());
+        // StringBuilder sb1 = new StringBuilder(c1.getPhoto().getPath());
+        // sb1.delete(0, 25);
+        // String correctPath1 = sb1.toString();
+        // model.addAttribute("Concert1Image", correctPath1);
+
+        // Concert c2 = concerts.get(1);
+        // model.addAttribute("Concert_2_Name", c2.getConcertName());
+        // StringBuilder sb2 = new StringBuilder(c2.getPhoto().getPath());
+        // sb2.delete(0, 25);
+        // String correctPath2 = sb2.toString();
+        // model.addAttribute("Concert2Image", correctPath2);
+
+        // Concert c3 = concerts.get(2);
+        // model.addAttribute("Concert_3_Name", c3.getConcertName());
+        // StringBuilder sb3 = new StringBuilder(c3.getPhoto().getPath());
+        // sb3.delete(0, 25);
+        // String correctPath3 = sb3.toString();
+        // model.addAttribute("Concert3Image", correctPath3);
+
+        int num = 1;
+        for (Concert c : concerts){
+            model.addAttribute("Concert" + num + "Name", c.getConcertName());
+            StringBuilder sb = new StringBuilder(c.getPhoto().getPath());
+            sb.delete(0, 25);
+            String correctPath = sb.toString();
+            model.addAttribute("Concert" + num + "Image", correctPath);
+            model.addAttribute("Concert" + num + "Date", c.getDateRange());
+            model.addAttribute("Concert" + num + "Time", c.getStartTime().toString());
+            num++;
+        }
+
+        // int num = 1;
+
+        // for (Concert c : concerts){
+        //     model.addAttribute("Concert" + num + "Name", c.getConcertName());
+        //     String createPoster = "src/main/resources/static/concert_posters/concert" + num + "poster.jpg";
+        //     FileUtils.writeByteArrayToFile(new File(createPoster), c.getPhoto());
+        //     String accessPoster = "concert_posters/concert" + num + "poster.jpg";
+        //     model.addAttribute("Concert" + num + "Image", accessPoster);
+        //     model.addAttribute("Concert" + num + "Date", c.getDate());
+        //     num++;
+        // }
+        
+        return "index";
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        return "test";
+    }
+
+
+    @PostMapping("/upload") // //new annotation since 4.3
+    public String singleFileUpload(@RequestParam("file") MultipartFile file,
+                                   RedirectAttributes redirectAttributes) {
+
+        if (file.isEmpty()) {
+            redirectAttributes.addFlashAttribute("message", "Please select a file to upload");
+            return "redirect:uploadStatus";
+        }
+
+        try {
+
+            // Get the file and save it somewhere
+            byte[] bytes = file.getBytes();
+            Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
+            Files.write(path, bytes);
+
+            redirectAttributes.addFlashAttribute("message",
+                    "You successfully uploaded '" + file.getOriginalFilename() + "'");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return "redirect:/uploadStatus";
+    }
+
+    @GetMapping("/uploadStatus")
+    public String uploadStatus() {
+        return "uploadStatus";
+    }
+
+    @RequestMapping("/shoppingcart.html")
+    public String viewShoppingCart(){
+        return "shoppingcart";
+    }
+
+}
