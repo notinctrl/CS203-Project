272052a1a501675e0af2d1f1f1363169507e900f
Kwon DaHee <dahee.kwon.2022@scis.smu.edu.sg>
2023-11-06 18:47:32 +0800

    modified test case for ticket

--- src/test/java/taylor/project/user/TicketServiceTest.java
+++ src/test/java/taylor/project/user/TicketServiceTest.java
@@ -19,9 +19,11 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.access.prepost.PreAuthorize;
 
+import taylor.project.concert.Concert;
 import taylor.project.ticket.Ticket;
 import taylor.project.ticket.TicketRepository;
 import taylor.project.ticket.TicketServiceImpl;
+import taylor.project.venue.Venue;
 
 @ExtendWith(MockitoExtension.class)
 public class TicketServiceTest {
@@ -34,7 +36,9 @@ public class TicketServiceTest {
 
     @Test
     void addTicket_available_Success(){
-        Ticket ticket = new Ticket("AA", "A1", 12, 123.0);
+        Venue venue = new Venue("s", 123, "sdfdsfsdfsd");
+        Concert concert = new Concert("monstax", 23, "s", "12:12:00", venue, "sd");
+        Ticket ticket = new Ticket(concert,"AA", "A1", 12, 123.0);
         List<Ticket> ticketList = new ArrayList<Ticket>();
         ticketList.add(ticket);
         //when(tickets.findTicketBySeatNo(ticket.getSeatNo())).thenReturn(ticketList);
@@ -52,6 +56,11 @@ public class TicketServiceTest {
         
         //verify(tickets).findTicketBySeatNo(ticket.getSeatNo());
 
+    }
+
+    @Test
+    void addTicket_Pending_Success(){
+        
     }
 
 }
